ca50dfd0b938e82da4000c77bb50786789856ca8
Adison Reddy <a.reddydesigns@gmail.com>
2023-04-06 15:50:29 -0500

    ABout page

--- PantrySquirrel/Controllers/HomeController.cs
+++ PantrySquirrel/Controllers/HomeController.cs
@@ -51,6 +51,11 @@ namespace PantrySquirrel.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
